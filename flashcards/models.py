7b1ee53091d69fa61b037932b59388d5205cb380
lorna <kimanilorna@yahoo.com>
2019-01-09 14:42:49 +0300

    adds the authenyication backend feature

--- flashcards/models.py
+++ flashcards/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
+import datetime as dt
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    '''A class to allow users to update and view their profile
+        '''
+    description = HTMLField()
+    username = models.ForeignKey(User,on_delete=models.CASCADE)
+    email = models.EmailField()
+    Flashcard = models.ForeignKey('Flashcard',on_delete=models.CASCADE,null=True)
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    def __str__(self):
+        return self.user.username
+class Flashcard(models.Model):
+    '''A class that allows users to post,view and rate projects
+        '''
+    
+    title = models.CharField(max_length=20)
+    description = HTMLField()
+    subject = models.CharField(max_length=50)
+    date = models.DateTimeField(auto_now_add=True)
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def save_card(self):
+        self.save()
+    def delte_card(self):
+        self.delete()
+
+    @classmethod
+    def update_card(self):
+        update_card = cls.objects.filter(id=id).update(description=description)
+        return update
+    def __str__(self):
+        return self.user.username
---
+++ flashcards/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from django.conf.urls import include
+from . import views
+
+urlpatterns=[
+    url(r'^accounts/profile',views.profile, name='profile'),
+    url(r'',views.index, name='index'),
+    url(r'^delete_card/(\d+)', views.delete_card ,name='delete'),
+    url(r'^post_card/',views.post_card, name='post'),
+    url(r'update_card/',views.update_card, name='update'),
+]
