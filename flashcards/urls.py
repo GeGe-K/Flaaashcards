1994b7e7bda799c8a16dc9115e20350f47b48227
GeGe-K <gloriagivondo@gmail.com>
2019-01-10 14:51:11 +0300

    configured the admin

--- flashcards/urls.py
+++ flashcards/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import include
 from . import views
 
 urlpatterns=[
+    url(r'^$', views.index, name='index'),
     url(r'^accounts/profile',views.profile, name='profile'),
-    url(r'',views.index, name='index'),
     url(r'^delete_card/(\d+)', views.delete_card ,name='delete'),
     url(r'^post_card/',views.post_card, name='post'),
     url(r'update_card/',views.update_card, name='update'),
